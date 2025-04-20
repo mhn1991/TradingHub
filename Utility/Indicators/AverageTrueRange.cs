72e45f6792d3a775cf99040642608bd1853e8d4b
Mohammad <mhn_1991@yahoo.com>
2025-04-20 22:41:35 +0100

    added the AverageTrueRange.cs to get the better stop/loss

--- Utility/Indicators/AverageTrueRange.cs
+++ Utility/Indicators/AverageTrueRange.cs
@@ -2,5 +2,18 @@ namespace Utility.Indicators;
 
 public class AverageTrueRange: Indicator
 {
-    
+    private int _window;
+
+    AverageTrueRange(int window = 14)
+    {
+        _window = window;
+    }
+
+    public void Calculate(bool isLive=false)
+    {
+        if (isLive)
+        {
+            
+        }
+    }
 }
